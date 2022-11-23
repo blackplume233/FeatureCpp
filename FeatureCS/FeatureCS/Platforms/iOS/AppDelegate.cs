2dd1ab5e05799a72487d6f9441157b37b6a12a7e
blackplume233 <blackplume233@gmail.com>
2022-11-23 11:31:23 +0800

    Init Project

---
+++ FeatureCS/FeatureCS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FeatureCS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
